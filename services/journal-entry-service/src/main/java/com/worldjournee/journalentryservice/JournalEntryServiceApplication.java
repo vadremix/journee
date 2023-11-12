10d12cb2da31c7f4da557e98376a56a44d1edf28
JL Roper <2345392+vadremix@users.noreply.github.com>
2023-11-12 05:43:01 +0100

    Add Java project skeletons

---
+++ services/journal-entry-service/src/main/java/com/worldjournee/journalentryservice/JournalEntryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.worldjournee.journalentryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalEntryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalEntryServiceApplication.class, args);
+	}
+
+}
---
+++ services/journal-entry-service/src/test/java/com/worldjournee/journalentryservice/JournalEntryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldjournee.journalentryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalEntryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
